384daa4164d66a8fa3d5d6f4d275b08daa39a4be
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2024-09-13 04:54:40 -0400

    added sentry packages and files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
-import { StrictMode } from 'react'
+//...
+import * as Sentry from "@sentry/react";
+import  { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+
+Sentry.init({
+  dsn: "https://169d9f37bdc63e47f6c3b6fa8bb7abfa@o4507944291663872.ingest.us.sentry.io/4507944294678528",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    // Sentry.reactRouterV6BrowserTracingIntegration({
+    //   useEffect: React.useEffect,
+    // }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
